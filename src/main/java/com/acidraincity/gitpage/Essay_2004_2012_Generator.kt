4e2e5d2d54385176f6a36cdec149f0480eeb204a
Yongtae Jeon <acidraincity@gmail.com>
2018-06-27 10:23:13 +0900

    !

--- src/main/java/com/acidraincity/gitpage/Essay_2004_2012_Generator.kt
+++ src/main/java/com/acidraincity/gitpage/Essay_2004_2012_Generator.kt
@@ -1,6 +1,7 @@
 package com.acidraincity.gitpage
 
 import com.acidraincity.tool.IOTool
+import org.apache.commons.lang3.StringUtils
 import java.io.File
 
 class Essay_2004_2012_Generator {}
@@ -8,10 +9,13 @@ class Essay_2004_2012_Generator {}
 fun main( args : Array< String > ){
 	File( "workspace/essay_2004_2012" ).listFiles{ f -> f.name.endsWith( ".jpg" ) }.forEach {
 		f ->
-		val str = """![](./${f.name})
+		val idx = Integer.parseInt(f.nameWithoutExtension)
+		val nextIdx = idx + 1
+		val str = """
+			|![](./${f.name})
 			|
-			|[다음](${Integer.parseInt(f.nameWithoutExtension) + 1})
+			|[다음](${StringUtils.leftPad("${nextIdx}", 3, '0' )})
 		""".trimMargin()
-		IOTool.writeStringToFile( File(f.nameWithoutExtension + ".md"), str, "UTF-8")
+		IOTool.writeStringToFile( File( f.parentFile, "${f.nameWithoutExtension}.md" ), str, "UTF-8")
 	}
 }
