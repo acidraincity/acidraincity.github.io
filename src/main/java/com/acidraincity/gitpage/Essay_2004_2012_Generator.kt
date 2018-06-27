365983d4c40692843da4ebe7669e00f9fdb9ec2e
Yongtae Jeon <acidraincity@gmail.com>
2018-06-27 10:09:51 +0900

    !

--- src/main/java/com/acidraincity/gitpage/Essay_2004_2012_Generator.kt
+++ src/main/java/com/acidraincity/gitpage/Essay_2004_2012_Generator.kt
@@ -1,7 +1,17 @@
 package com.acidraincity.gitpage
 
+import com.acidraincity.tool.IOTool
+import java.io.File
+
 class Essay_2004_2012_Generator {}
 
 fun main( args : Array< String > ){
-	println( "!!" )
+	File( "workspace/essay_2004_2012" ).listFiles{ f -> f.name.endsWith( ".jpg" ) }.forEach {
+		f ->
+		val str = """![](./${f.name})
+			|
+			|[다음](${Integer.parseInt(f.nameWithoutExtension) + 1})
+		""".trimMargin()
+		IOTool.writeStringToFile( File(f.nameWithoutExtension + ".md"), str, "UTF-8")
+	}
 }
